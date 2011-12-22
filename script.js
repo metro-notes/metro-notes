39b07685d0e810832e731ae9b7b3f51f27b81f91
Chuma Naji <cnnaji2003@gmail.com>
2011-12-21 21:29:26 -0800

    Added button that slides to toggle overlay with a slideDown effect

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var displayOverlay = function() {
-    document.body.style.background='red !important';
+    var overlaySelector = '#metro-notes-overlay';
+    var overlayObject = $(overlaySelector);
+    if(overlayObject.length) {
+        overlayObject.slideToggle("fast");
+    } else {
+        $('body').append('<div id="metro-notes-overlay"></div>');
+        $(overlaySelector).hide().slideDown("fast");
+    }
 }
