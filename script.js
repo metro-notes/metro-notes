5f3e9fc2b4dccb6135cbf9a3d23cf1230416026a
Chuma Naji <cnnaji2003@gmail.com>
2011-12-21 21:49:45 -0800

    Inject scripts on page load

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
+var overlaySelector = '#metro-notes-overlay';
+$('body').append('<div id="metro-notes-overlay"></div>');
+$(overlaySelector).hide();
+
 var displayOverlay = function() {
-    var overlaySelector = '#metro-notes-overlay';
     var overlayObject = $(overlaySelector);
-    if(overlayObject.length) {
-        overlayObject.slideToggle("fast");
-    } else {
-        $('body').append('<div id="metro-notes-overlay"></div>');
-        $(overlaySelector).hide().slideDown("fast");
-    }
+    overlayObject.slideToggle("fast");
 }
