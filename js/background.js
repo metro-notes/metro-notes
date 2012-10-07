c1137befc0ab49c9456885f3847005cc741d2fde
Chuma Nnaji <cnnaji2003@gmail.com>
2012-10-06 19:20:08 -0700

    Refactored and cleaned up
    Upgraded to manifest v2.
    Changed background pages to event pages.
    Changed settings and notes into a collection (rather than discrete)
    Using chrome.storage.sync.
    Hella refactoring.
    Using mousetrap.

--- js/background.js
+++ js/background.js
@@ -1,21 +1,51 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null, {code:"displayOverlay()"});
+    chrome.tabs.executeScript(null, {code:'displayOverlay()'});
 });
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	if(request.cmd == "save") {
-		localStorage.setItem('toggleKey', request.data.toggleKey);
-	}
-
-	if(request.cmd == "load") {
-		sendResponse(localStorage.getItem('toggleKey'));
-	}
-
-	if(request.cmd == "badge") {
-		if(request.data.count > 0){
-			chrome.browserAction.setBadgeText({'text': '' + request.data.count, 'tabId': sender.tab.id});
+chrome.runtime.onInstalled.addListener(function() {
+	chrome.storage.sync.get('notes', function(res) {
+		if(!res.notes) {
+			chrome.storage.sync.set({ notes: {} });
 		}
-	}
+	});
 });
+
+chrome.extension.onMessage.addListener(
+	function(message, sender, callback) {
+		if(message.cmd == 'saveSettings') {
+			chrome.storage.sync.set({ 
+				settings: {
+					toggleKey: message.data.toggleKey
+				}
+			});
+		}
+
+		if(message.cmd == 'loadSettings') {
+			chrome.storage.sync.get('settings', function(res) {
+				callback(res.settings);
+			});
+		}
+
+		if(message.cmd == 'badge') {
+			if(message.data.count > 0){
+				chrome.browserAction.setBadgeText({'text': '' + message.data.count, 'tabId': sender.tab.id});
+			}
+		}
+
+		if (message.cmd == 'loadNotes') {
+			chrome.storage.sync.get('notes', function(res) {
+				callback(res.notes[message.data.url]);
+			});
+		}
+
+		if (message.cmd == 'saveNotes') {
+			chrome.storage.sync.get('notes', function(res) {
+				res.notes[message.data.url] = message.data.notes;
+				chrome.storage.sync.set(res);
+			});
+		}
+		return true;
+	}
+);
 
--- js/script.js
+++ js/script.js
@@ -16,8 +16,12 @@ var displayOverlay = function() {
 	}
 };
 
-//Inserts a new note based on the given noteObj and index.
-//Only adds the note to the DOM (does not affect localStorage).
+/**
+	Inserts a new note based on the given noteObj and index.
+	Only adds the note to the DOM (does not affect storage).
+	noteObj: The note to be inserted.
+	index: The index of the note in the storage.
+*/
 var insertNote = function(noteObj, index) {
     var note = $('<div class="metro-notes-note" id="metro-notes-note-' + index + '"><div id="delete-' + index + '" class="delete options"></div><div id="handle-' + index + '"></div><p></p></div>');
     //Append the note contents into the inner <p> tag of the new <div>
@@ -34,17 +38,19 @@ var insertNote = function(noteObj, index) {
     });
     
     //Assign drag and dragend event handlers
-	note.drag(function(ev, dd){
+	note.drag(function(ev, dd) {
 		$(this).css({
 			top: dd.offsetY,
 			left: dd.offsetX
 		});
-	}, {
-        'handle': '.handle'
-    });
+		},
+		{
+	        'handle': '.handle'
+	    }
+	);
 
 	//Function called when an element is dropped after dragging.
-	//Updates the elements new x, y location in localStorage
+	//Updates the elements new x, y location in storage
 	note.on('dragend', function() {
 		var tar = $(this);
 		noteObj = {
@@ -54,21 +60,24 @@ var insertNote = function(noteObj, index) {
 		'width': tar.css('width'),
 		'height': tar.css('height')
 		};
-		updateNote(tar.attr('id').replace('metro-notes-note-', ''), noteObj);
+		updateNote(noteObj, tar.attr('id').replace('metro-notes-note-', ''));
 	});
 	
-	//Delete notes when delte is clicked.
+	//Delete notes when delete is clicked.
 	note.on('click', '.delete', function() {
 		deleteNote($(this).prop('id').replace('delete-', ''));
 	});
 
     overlayObject.append(note);
-	//console.log("note created");
     return note;
 };
 
-//Update an existing note based on its index to match the given noteObj
-var updateNote = function(index, noteObj) {
+/**
+	Update an existing note based on its index to match the given noteObj.
+	noteObj: The note being editing.
+	index: The index of the note object.
+*/
+var updateNote = function(noteObj, index) {
     $('#metro-notes-' + index).text(noteObj.note);
     $('#metro-notes-note-' + index).css({
         'top': noteObj.top,
@@ -77,97 +86,64 @@ var updateNote = function(index, noteObj) {
         'height': noteObj.height
     });
     notes[index] = noteObj;
-    localStorage.setItem(url, JSON.stringify(notes));
+    saveNotes(url, notes);
 };
 
-//Delete the note specified by the given index
-//Removes it from the DOM and removes it from the localStorage
+/**
+	Delete the note specified by the given index
+	Removes it from the DOM and removes it from the storage.
+*/
 var deleteNote = function(index) {
     $('#metro-notes-note-' + index).remove();
     notes[index] = '';
-    localStorage.setItem(url, JSON.stringify(notes));
-	
-	//console.log("note deleted");
+    saveNotes(url, notes);
 };
 
-//logic to set key
- var getToggleKey = function(e){
-		 //console.log(e);
-	 if(e.which === 16 || e.which === 17 || e.which === 18 || e.which === 91 || e.which === 92) {
-		 $('body').one('keydown', getToggleKey);
-		 return false;
-	 }
-	 var text = '';
-	 if(e.altKey)
-		 text += 'alt+';
-	 if(e.ctrlKey)
-		 text += 'ctrl+';
-	 if(e.metaKey)
-		 text += 'meta+';
-	 if(e.shiftKey)
-		 text += 'shift+';
-
-	 //need alternative method to this, '.' shows up as '3/4' symbol
-	 text += getCharDesc(e.which).toLowerCase();
-	 saveSetting(text);
-	 //localStorage.setItem('toggleKey', text);
-	 $('#toggle-key').data('click-count', false);
-	 $('#toggle-key').text(text);
-	 loadCurrentSetting();
-	 return false;
- }
-
-//loads default settings on first execution
-var loadDefaultSetting = function(){
-	//console.log("load default settings");
-	saveSetting('');
-	//localStorage.setItem('toggleKey', '');
-	$('#toggle-key').text('');
-	
-	//not working
-	$(document).unbind('keydown');
-};
-
-//loads at start of every execution except for first
-var loadCurrentSetting = function(){
-	//console.log("load current settings");
-	loadSetting(function(key) {
-		//var key = localStorage.getItem('toggleKey');
-		if(!key) {
-			loadDefaultSetting();
+var loadSettings = function() {
+	chrome.extension.sendMessage({cmd: 'loadSettings'}, function(settings) {
+		if (settings) {
+			if(!settings.toggleKey || settings.toggleKey == '') {
+				$('#toggle-key').text('');
+				Mousetrap.reset();
+			} else {
+				$('#toggle-key').text(settings.toggleKey);
+				Mousetrap.reset();
+				Mousetrap.bind(settings.toggleKey, displayOverlay);
+			}
 		}
-		//key = localStorage.getItem('toggleKey');
-		if(key === '') {
-			return;
-		}
-		$('#toggle-key').text(key);
-		$(document).unbind('keydown');
-		$(document).bind('keydown', key, displayOverlay);
 	});
-};
-
-var getCharDesc = function(char_code)
-{
-	var specialKeys = {
-		8: "backspace", 9: "tab", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
-		20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
-		37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del", 
-		96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
-		104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/", 
-		112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8", 
-		120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 188: ",", 190: ".",
-		191: "/", 224: "meta"
-	};
-	//console.log(char_code);
-	return specialKeys[char_code] ? specialKeys[char_code] : String.fromCharCode(char_code);
 }
 
-var saveSetting = function(toggleKey) {
-	chrome.extension.sendRequest({cmd: "save", data: {'toggleKey': toggleKey }});
+var saveSettings = function(toggleKey) {
+	chrome.extension.sendMessage({cmd: 'saveSettings', data: {toggleKey: toggleKey }});
+	loadSettings();
 } 
 
-var loadSetting = function(func) {
-	chrome.extension.sendRequest({cmd: "load"}, func);
+//If there is a URL, get the list of notes from the storage
+//Why would there NOT be a url?
+var loadNotes = function() {
+	if(url.length) {
+		chrome.extension.sendMessage({cmd: 'loadNotes', data: {url: url }}, function(newNotes) {
+			//If notes exist, for each one, insert it into the page.
+			if(newNotes !== null) {
+				//note_arr = [];
+				for(i = 0; i < newNotes.length; i++) {
+					if(newNotes[i] !== '') {
+						//note_arr.push(notes[i]);
+						//insertNote(note_arr[note_arr.length-1], note_arr.length-1);
+						insertNote(newNotes[i], i);
+					}
+				}
+				notes = newNotes;
+				chrome.extension.sendMessage({cmd: 'badge', data: {count: notes.length }});
+			}
+		});
+	}
+}
+
+var saveNotes = function(url, notes) {
+	chrome.extension.sendMessage({cmd: 'saveNotes', data: {url: url, notes: notes }});
+	chrome.extension.sendMessage({cmd: 'badge', data: {count: notes.length }});
 }
 
 //Execute on every page load.
@@ -178,38 +154,19 @@ var init = function() {
 	overlayObject.hide();
 
 	overlayObject.append('<div id="instant_issue">Sorry...Metro Notes does not work well with Google Instant. We are investigating a fix...</div>');
-	overlayObject.append("<div class='wrench' id='wrench'>wrench</div>");
-	overlayObject.append("<div class='wrench' id='toggle-key-label'>toggle key<span id='toggle-key'></span></div>");
-	overlayObject.append("<div class='wrench' id='reset'>reset</div>");
+	overlayObject.append('<div class="wrench" id="wrench">wrench</div>');
+	overlayObject.append('<div class="options toggle-key-label" id="toggle-key-label">toggle key<span id="toggle-key"></span></div>');
+	overlayObject.append('<div class="options" id="reset">reset</div>');
 
 	$('#instant_issue').hide();
 	$('#toggle-key-label').hide();
 	$('#reset').hide();
 		
-	loadCurrentSetting();
-
-	//If there is a URL, get the list of notes from the localStorage
-	if(url.length) {
-		var arr = localStorage.getItem(url);
-		//If notes exist, for each one, insert it into the page.
-		if(arr !== null) {
-			notes = JSON.parse(arr);
-			note_arr = [];
-			for (var i in notes) {
-				if(notes[i] !== '') {
-					note_arr.push(notes[i]);
-					insertNote(note_arr[note_arr.length-1], note_arr.length-1);
-				}
-			}
-			notes = note_arr;
-			chrome.extension.sendRequest({cmd: "badge", data: {'count': notes.length }});
-			localStorage.setItem(url, JSON.stringify(notes));
-		}
-	}
+	loadSettings();
+	loadNotes();
 
 	//If clicked, insert a new note at the page that was clicked.
-	overlayObject.on('click', function (e) {
-		//console.log(url);
+	overlayObject.on('click', function(e) {
 		var ajax_url = document.URL;     //Page URL #Necessary cos Ajax
 
 		if(ajax_url.indexOf('#') !== -1 && (ajax_url.indexOf('www.google.com/search') !== -1 || ajax_url.indexOf('www.google.com/webhp' !== -1))) {
@@ -218,7 +175,7 @@ var init = function() {
 		}
 
 		//If in insert mode, first exit insert mode, before inserting a new note.
-		if(insertMode){
+		if(insertMode) {
 			insertMode = false;
 			return false;
 		}
@@ -228,69 +185,59 @@ var init = function() {
 		return false;
 	});
 
-	//Clicking on the note should make its children editable (especially now we have the drag handlers)
-	overlayObject.on('click', '.metro-notes-note', function () {
+	//Clicking on the note should make its children editable(especially now we have the drag handlers)
+	overlayObject.on('click', '.metro-notes-note', function() {
 		$(this).children('p').prop('contentEditable', 'true').focus();
 		insertMode = true;
 		return false;
 	});
 
 	//On mouseover, bring the note to the front
-	overlayObject.on('mouseover', '.metro-notes-note', function () {
+	overlayObject.on('mouseover', '.metro-notes-note', function() {
 		$(this).css('zIndex', lastzindex);
 		lastzindex++;
 	});
 
 	//When the note loses focus, and it has data, save it.
-	overlayObject.on('blur', '.metro-notes-note > p', function () {
+	overlayObject.on('blur', '.metro-notes-note > p', function() {
 		var note = $(this).text();
 		//If the note is empty, delete it.
 		if($.trim(note) === '') {
 			try {
 				deleteNote($(this).parent('.metro-notes-note').attr('id').replace('metro-notes-note-', ''));
-			} catch (e) { console.log(e); }
+			} catch(e) { console.log(e); }
 		} else {
 			var tar = $(this).parent('.metro-notes-note');
 			noteObj = {
 				'note': tar.text(),
-		'top': tar.css('top'),
-		'left': tar.css('left'),
-		'width': tar.css('width'),
-		'height': tar.css('height')
+				'top': tar.css('top'),
+				'left': tar.css('left'),
+				'width': tar.css('width'),
+				'height': tar.css('height')
 			};
-			updateNote(tar.attr('id').replace('metro-notes-note-', ''), noteObj);
+			updateNote(noteObj, tar.attr('id').replace('metro-notes-note-', ''));
 			$(this).prop('contentEditable', 'false');
 		}
 		return false;
 	});
 
-	$('#wrench').on('click',function(e){
-		//console.log("wrench clicked");
-		$('#reset').slideToggle('fast');
-		$('#toggle-key-label').slideToggle('fast');
+	$('#wrench').on('click',function() {
 		$('.options').slideToggle('fast');
 		return false;
 	});
 	
-	$('#reset').on('click', function(){
-		//console.log("resetting...");
-		loadDefaultSetting();
-		
+	$('#reset').on('click', function() {
+		saveSettings('');
 		return false;
 	});
 
 	//on click, user is prompted to set toggle key
 	//If the user clicks again, nothing is changed
-	$('#toggle-key-label').on('click', function(e){
+	$('#toggle-key-label').on('click', function(e) {
 		if($('#toggle-key').data('click-count') === true) {
 			$('#toggle-key').data('click-count', false);
-			loadSetting(function(key) {
-				console.log(key);
-				$('#toggle-key').text(key);
-			});	
-			$('body').off('keydown', getToggleKey);
+			loadSettings();
 		} else {
-			//console.log("toggle clicked, waiting for user to hit key");
 			$('#toggle-key').data($('#toggle-key').text());
 			$('#toggle-key').data('click-count', true);
 			$('#toggle-key').text('new hotkey...');
@@ -299,5 +246,42 @@ var init = function() {
 		return false;
 	});
 };
+
+//logic to set key
+var getToggleKey = function(e){
+	if(e.which === 16 || e.which === 17 || e.which === 18 || e.which === 91 || e.which === 92) {
+		$('body').one('keydown', getToggleKey);
+		return false;
+	}
+	var text = '';
+	if(e.altKey)
+		text += 'alt+';
+	if(e.ctrlKey)
+		text += 'ctrl+';
+	if(e.metaKey)
+		text += 'meta+';
+	if(e.shiftKey)
+		text += 'shift+';
+
+	//need alternative method to this, '.' shows up as '3/4' symbol
+	text += getCharDesc(e.which).toLowerCase();
+	$('#toggle-key').data('click-count', false);
+	saveSettings(text);
+	return false;
+}
+
+var getCharDesc = function(char_code) {
+	var specialKeys = {
+		8: "backspace", 9: "tab", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
+		20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
+		37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del", 
+		96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
+		104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/", 
+		112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8", 
+		120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 188: ",", 190: ".",
+		191: "/", 224: "meta"
+	};
+	return specialKeys[char_code] ? specialKeys[char_code] : String.fromCharCode(char_code);
+}
 
 init();
